a47960461b3615bc697b38ea8c150bf407e18776
ihadyy <hade.hijaze1010@gmail.com>
2024-09-25 17:04:56 +0300

    frfrfrrfr

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,53 +18,50 @@ public class MainActivity extends AppCompatActivity {
     private ImageView img;
     private boolean play=true;
     private float x,y;
-    private TextView txt;
+    private TextView txt2;
     private Button stop;
     private RadioButton level1,level2,level3;
+    private Dialog d;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        stop =findViewById(R.id.stop);
        level1=findViewById(R.id.level1);
        level2=findViewById(R.id.level2);
        level3=findViewById(R.id.level3);
+       stop=findViewById(R.id.stop);
+        d = new Dialog(this);
+        d.setContentView(R.layout.points);
+        d.setCancelable(true);
+
+
        level1.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
                levelSpeed=500;
-               counet=1;
            }
        });
         level2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 levelSpeed=200;
-                counet=1;
             }
         });
         level3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 levelSpeed=100;
-                counet=1;
             }
         });
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play=false;
-            }
-        });
-        txt = findViewById(R.id.txt);
+
+
                 img=findViewById(R.id.emojy);
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txt.setText(counet+"");
-                counet++;
+                counet=counet+1;
             }
         });
         thread=new Thread(()->{
@@ -76,6 +75,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         thread.start();
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play=false;
+                txt2=d.findViewById(R.id.txt2);
+                txt2.setText(counet+"");
+                d.show();
+            }
+        });
     }
     private void srartMoveImage(){
         x = (float) Math.random()*800;
